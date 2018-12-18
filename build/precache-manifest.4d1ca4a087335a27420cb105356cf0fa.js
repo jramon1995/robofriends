0223d9841ce71852e9f3db514eb9025cddee4472
Ramon <joserrll1995@gmail.com>
2018-12-18 01:33:31 -0600

    First commit

---
+++ build/precache-manifest.4d1ca4a087335a27420cb105356cf0fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce93e229027b282cc88c",
+    "url": "/static/js/main.ce93e229.chunk.js"
+  },
+  {
+    "revision": "3ff641f41768311c7505",
+    "url": "/static/js/1.3ff641f4.chunk.js"
+  },
+  {
+    "revision": "ce93e229027b282cc88c",
+    "url": "/static/css/main.7427d91e.chunk.css"
+  },
+  {
+    "revision": "3ff641f41768311c7505",
+    "url": "/static/css/1.9b5c2575.chunk.css"
+  },
+  {
+    "revision": "d91add7f3525209ee5a0257328c3bf47",
+    "url": "/index.html"
+  }
+];
